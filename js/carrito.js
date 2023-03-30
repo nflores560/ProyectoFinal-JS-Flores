61d2a0ca225895bc5d7044dc1208f9d078bf22e9
nflores560 <natus_22@hotmail.com>
2023-03-30 04:28:14 -0300

    subiendo proyecto final

--- js/carrito.js
+++ js/carrito.js
@@ -30,6 +30,7 @@ carrito.forEach((product) => {
         <p>Cantidad: ${product.cantidad}</p>
         <span class="sumar"> + </span>
         <p>Total: ${product.cantidad * product.precio}</p>
+        <span class="delete-product"> ❌ </span>
         `;
 
     modalContainer.append(carritoContent);
@@ -51,15 +52,14 @@ carrito.forEach((product) => {
         saveLocal();
         pintarCarrito();
 
+    });
+
+    let eliminar = carritoContent.querySelector(".delete-product");
+
+    eliminar.addEventListener("click", ()=> {
+        eliminarProducto(product.id);
     })
 
-
-    let eliminar = document.createElement("span");
-    eliminar.innerText = "❌";
-    eliminar.className = "delete-product";
-    carritoContent.append(eliminar);
-
-    eliminar.addEventListener("click", eliminarProducto);
 });
 
 
@@ -74,8 +74,10 @@ modalContainer.append(totalBuying);
 
 verCarrito.addEventListener("click", pintarCarrito);
 
-const eliminarProducto = () => {
-    const foundId = carrito.find((element) => element.id);
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
+
+    console.log(foundId);
 
     carrito = carrito.filter((carritoId) => {
         return carritoId !== foundId;
--- js/main.js
+++ js/main.js
@@ -5,49 +5,59 @@ const cantidadCarrito = document.getElementById("cantidadCarrito")
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-alimentos.forEach((product)=> {
-    let content = document.createElement("div");
-    content.className = "card";
-    content.innerHTML = `
-    <img src="${product.img}">
-    <h3>${product.nombre}</h3>
-    <p class="price">${product.precio} $</p>
-    `;
+const getProducts = async () => {
+    const response = await fetch("data.json");
+    const data = await response.json();
 
-    shopContent.append(content);
-
-    let comprar = document.createElement("button")
-    comprar.innerText = "Comprar";
-    comprar.className = "comprar";
-
-
-    content.append(comprar);
-
-    comprar.addEventListener("click", () =>{
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-
-    if(repeat){
-        carrito.map((prod) => {
-            if(prod.id === product.id){
-                prod.cantidad++;
-            }
-        });
-    }else {
-        carrito.push({
-            id: product.id,
-            img: product.img,
-            nombre: product.nombre,
-            precio: product.precio,
-            cantidad: product.cantidad,
-        });
+    alimentos.forEach((product)=> {
+        let content = document.createElement("div");
+        content.className = "card";
+        content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="price">${product.precio} $</p>
+        `;
     
-        console.log(carrito);
-        console.log(carrito.length);
-        carritoCounter();
-        saveLocal();
-    }
+        shopContent.append(content);
+    
+        let comprar = document.createElement("button")
+        comprar.innerText = "Comprar";
+        comprar.className = "comprar";
+    
+    
+        content.append(comprar);
+    
+        comprar.addEventListener("click", () =>{
+        const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+    
+        if(repeat){
+            carrito.map((prod) => {
+                if(prod.id === product.id){
+                    prod.cantidad++;
+                }
+            });
+        }else {
+            carrito.push({
+                id: product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
+            });
+        
+            console.log(carrito);
+            console.log(carrito.length);
+            carritoCounter();
+            saveLocal();
+        }
+        });
     });
-});
+    
+};
+
+getProducts();
+
+
 
 //set item
 const saveLocal = () => {
